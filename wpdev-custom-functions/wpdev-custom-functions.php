c4c594cf9d5c920693e2d0e9fdeb172f9b296535
skyrepprac <viveks@e2msol.com>
2017-08-22 20:18:13 +0530

    Update wpdev-custom-functions.php

--- wpdev-custom-functions/wpdev-custom-functions.php
+++ wpdev-custom-functions/wpdev-custom-functions.php
@@ -1,1 +1,14 @@
+<?php
 
+/**
+* Plugin Name: Custom Functions Plugin
+* Description: This plugin contains all of my awesome custom functions.
+* Author: Wp Dev
+* Version: 0.1
+*/
+
+/* Your code goes below here. */
+
+/* Your code goes above here. */
+
+?>
